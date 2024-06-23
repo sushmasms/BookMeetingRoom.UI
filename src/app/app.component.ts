8531042e33401f6021eb5547744b7c0d0a9507dd
mssushmx <sushmax.ms@intel.com>
2024-06-24 03:09:06 +0530

    BookMeetingRoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'MeetingRoom.UI';
+  constructor(
+    private router: Router,
+  ) {}
+  ngOnInit() {
+    this.router.navigate(['/'])
+  }
 }
